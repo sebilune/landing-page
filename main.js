71d5599389468a20c376072a0ce4404e23954cf5
Sebastian Ramirez <sebastianramord@gmail.com>
2023-05-23 13:46:17 -0500

    Updated and fixed bugs with the buttons. Might've changed the color palette again.

---
+++ main.js
@@ -0,0 +1,7 @@
+const lightbox = GLightbox({
+    'href': './img/exolink.mp4',
+    'type': 'video',
+    'source': 'local', //vimeo, youtube or local
+    'width': 900,
+    'autoplayVideos': true,
+});
