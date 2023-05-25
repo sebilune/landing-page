b91ece1c45b0e51e9c8ee1093fcf253ef2a475bb
Sebastian Ramirez <sebastianramord@gmail.com>
2023-05-25 06:22:08 -0500

    Made changes to main.js Created an event listener that when screen size is less than 768px, the columns will re-arrange themselves to present a more responsive look.

--- main.js
+++ main.js
@@ -7,16 +7,35 @@ const lightbox = GLightbox({
   autoplayVideos: true,
 });
 
+// Copy to Clipboard Button
 document.getElementById("copyButton").addEventListener("click", function() {
-    var content = document.getElementById("contentToCopy").innerText;
+  var content = document.getElementById("contentToCopy").innerText;
 
-    navigator.clipboard.writeText(content)
-      .then(function() {
-        // Clipboard successfully set
-        alert("Content copied to clipboard: " + content);
-      })
-      .catch(function(error) {
-        // An error occurred
-        alert("Failed to copy content to clipboard: " + error);
-      });
+  navigator.clipboard.writeText(content)
+  .then(function() {
+    // Clipboard successfully set
+    alert("Content copied to clipboard: " + content);
+  })
+  .catch(function(error) {
+    // An error occurred
+    alert("Failed to copy content to clipboard: " + error);
   });
+});
+
+
+// Arrange "Services" section accordingly when screen size is less than 768px.
+window.addEventListener("resize", function () {
+  const marketing = document.getElementById("marketing").classList
+  const cloudHosting = document.getElementById("cloud-hosting").classList
+
+  
+  if(window.innerWidth < 768) {   
+    marketing.add("d-flex", "flex-column", "flex-column-reverse");
+    cloudHosting.add("d-flex", "flex-column", "flex-column-reverse"); 
+  } 
+
+  else{
+    marketing.remove("d-flex", "flex-column", "flex-column-reverse");
+    cloudHosting.remove("d-flex", "flex-column", "flex-column-reverse"); 
+  }
+});
